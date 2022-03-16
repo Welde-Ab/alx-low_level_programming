6a1f77a64ee555284615aded5a250d872682223c
Welde-Ab <35730399+Welde-Ab@users.noreply.github.com>
2022-03-16 09:55:16 +0000

    7-last corrected negative number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,13 @@
 #include "main.h"
+#include <math.h>
 
 /**
  * print_last_digit - check the code
  * @num: input to the program
  * Return: last digit of num
  */
-int print_last_digit (int num)
+int print_last_digit(int num)
 {
-return (num % 10);
+int last = abs(num) % 10;
+return (last * 10 + last);
 }

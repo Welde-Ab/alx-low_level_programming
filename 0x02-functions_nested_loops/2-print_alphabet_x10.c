bad871a9c5d8ae41949a03e4666ad3423fa910bb
Welde-Ab <35730399+Welde-Ab@users.noreply.github.com>
2022-03-16 04:48:20 +0000

    2-print_alphabet_10_times - Updated

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,17 @@
 #include "main.h"
 /**
- * main - simply prints _putchar
- * Description: main funciton prints the character _putchar
- * Return: Program returns 0
+ * print_alphabet_x10 - prints times the alphabet
+ * Description: Prints 10 times the alphabet
+ * Return: void
  */
-int main(void)
+void print_alphabet_x10(void)
 {
-print_alphabet_x10();
-return (0);
+for (int i=0; i<10;i++)
+{
+for (char lett = 'a'; lett <= 'z'; lett++)
+{
+putchar(lett);      
+}
+putchar('\n'); 
+}
 }

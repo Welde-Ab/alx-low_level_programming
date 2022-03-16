a15edd21da7010d60cdd52380fff92b2fb01a269
Welde-Ab <35730399+Welde-Ab@users.noreply.github.com>
2022-03-16 04:00:49 +0000

    Space after while

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * print_alphabet - simply prints _putchar
- * Description: a function that prints the alphabet, in lowercase, followed by a new line.
+ * Description: a function that prints the alphabet
  * Return: Program returns 0
  */
 void print_alphabet(void)
 {
 int ascii = 97;
-while(ascii<=122)
+while (ascii <= 122)
 {
 _putchar((char)ascii);
 ascii++;

7daa086f3c65d7012c863987c4a8d3eee5b72a33
Welde-Ab <35730399+Welde-Ab@users.noreply.github.com>
2022-03-16 09:39:22 +0000

    6-abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,21 +2,22 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _abs - computes absolute value
+ * @num: number from user
+ * Return: absolute value of input
  */
-int main(void)
+int _abs(int num)
 {
-  int r;
-
-  r = _abs(-1);
-  printf("%d\n", r);
-  r = _abs(0);
-  printf("%d\n", r);
-  r = _abs(1);
-  printf("%d\n", r);
-  r = _abs(-98);
-  printf("%d\n", r);
-  return (0);
+if (num < 0)
+{
+return (-num);
+}
+else if (num == 0)
+{
+return (0);
+}
+else (num > 0)
+{
+return (num);
+}
 }

5a46ab31c3167e7b87469ff8afcf389fd8d445c1
Welde-Ab <35730399+Welde-Ab@users.noreply.github.com>
2022-03-16 05:22:46 +0000

    3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,18 @@
 #include "main.h"
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * _islower - a function that checks for lowercase character
+ * @c: character to be checked
+ * Return: O if not lower & 1 if lower
  */
-int main(void)
+int _islower(int c)
 {
-  int r;
-
-  r = _islower('H');
-  _putchar(r + '0');
-  r = _islower('o');
-  _putchar(r + '0');
-  r = _islower(108);
-  _putchar(r + '0');
-  _putchar('\n');
-  return (0);
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+else
+{
+return (0);  
+}
 }

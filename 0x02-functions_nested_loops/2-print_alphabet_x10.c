61d76c5651f99e306c1a12415e62ca03b1bf0f37
Welde-Ab <35730399+Welde-Ab@users.noreply.github.com>
2022-03-16 04:54:09 +0000

    Removed declaration

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,9 +6,11 @@
  */
 void print_alphabet_x10(void)
 {
-for (int i=0; i<10;i++)
+int i;
+char lett;
+for (i=0; i<10;i++)
 {
-for (char lett = 'a'; lett <= 'z'; lett++)
+for (lett = 'a'; lett <= 'z'; lett++)
 {
 putchar(lett);      
 }

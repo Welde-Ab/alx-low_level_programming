527ae50d422e960d7c55447274665f06a20678bb
Welde-Ab <35730399+Welde-Ab@users.noreply.github.com>
2022-03-16 09:45:25 +0000

    7.last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,18 +1,11 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_last_digit - check the code
+ * @num: input to the program
+ * Return: last digit of num
  */
-int main(void)
+int print_last_digit (int num)
 {
-  int r;
-
-  print_last_digit(98);
-  print_last_digit(0);
-  r = print_last_digit(-1024);
-  _putchar('0' + r);
-  _putchar('\n');
-  return (0);
+return (num % 10);
 }

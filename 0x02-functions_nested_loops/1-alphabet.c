fb7d112929fea42d8a925e42ae838edfa249a2a0
Welde-Ab <35730399+Welde-Ab@users.noreply.github.com>
2022-03-16 02:39:16 +0000

    1-main

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - simply prints _putchar
+ * Description: main funciton prints the character _putchar
+ * Return: Program returns 0
+ */
+int main(void)
+{
+print_alphabet();
+return (0);
+}
+
+void print_alphabet(void)
+{
+int ascii = 97;
+while(ascii<=122)
+{
+putchar(char(ascii));
+}
+}

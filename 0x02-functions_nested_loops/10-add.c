9753430724fc7213cd4a2c7c5f0379b6a3d7cea3
Welde-Ab <35730399+Welde-Ab@users.noreply.github.com>
2022-03-17 02:38:07 +0000

    10-add What addition

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,16 +1,13 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * add - returns the sum of its parameters
+ * @a: int type number
+ * @b: int type number
+ * Return:0
  */
-int main(void)
-{
-  int n;
 
-  n = add(89, 9);
-  printf("%d\n", n);
-  return (0);
+int add(int a, int b)
+{
+return (a + b);
 }

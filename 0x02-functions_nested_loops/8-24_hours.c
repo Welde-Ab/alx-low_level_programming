dd1c88b0fff75cc7fe19f83b759f6ed744916e1e
Welde-Ab <35730399+Welde-Ab@users.noreply.github.com>
2022-03-17 02:20:36 +0000

    8-jack fixed '0'

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,21 +16,21 @@ for (j = 0; j < 60; j++)
 if (i < 10)
 {
 _putchar('0');
-_putchar(i);
+_putchar(i + '0');
 }
 else
 {
-_putchar(i);
+_putchar(i + '0');
 }
 _putchar(':');
 if (j < 10)
 {
 _putchar('0');
-_putchar(j);
+_putchar(j + '0');
 }
 else
 {
-_putchar(j);
+_putchar(j + '0');
 }
 _putchar('\n');
 }

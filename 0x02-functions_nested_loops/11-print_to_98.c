d4cd35608192407395f252600014ceb99e034708
Welde-Ab <35730399+Welde-Ab@users.noreply.github.com>
2022-03-17 02:41:53 +0000

    11-to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,16 +1,24 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_to_98 - print to 98
+ * @n : number to start from
+ * Return:0 or 1
  */
-int main(void)
+
+void print_to_98(int n)
 {
-  print_to_98(0);
-  print_to_98(98);
-  print_to_98(111);
-  print_to_98(81);
-  print_to_98(-10);
-  return (0);
+while (n < 98)
+{
+printf("%i, ", n);
+n++;
+}
+while (n > 98)
+{
+printf("%i, ", n);
+n--;
+}
+printf("98");
+putchar('\n');
 }

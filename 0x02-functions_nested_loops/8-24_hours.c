d2fb3a841a803c4b60a22acd67b60fd8fc267dbb
Welde-Ab <35730399+Welde-Ab@users.noreply.github.com>
2022-03-17 02:28:10 +0000

    8-jack iterative %10

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,6 +9,7 @@ void jack_bauer(void)
 {
 int i;
 int j;
+int temp;
 for (i = 0; i < 24; i++)
 {
 for (j = 0; j < 60; j++)
@@ -20,7 +21,11 @@ _putchar(i + '0');
 }
 else
 {
-_putchar(i + '0');
+temp = i % 10;
+temp = i - temp;
+temp /= 10;
+_putchar(temp + '0');
+_putchar((i % 10) + '0');
 }
 _putchar(':');
 if (j < 10)
@@ -30,7 +35,11 @@ _putchar(j + '0');
 }
 else
 {
-_putchar(j + '0');
+temp = j % 10;
+temp = j - temp;
+temp /= 10;
+_putchar(temp + '0')
+_putchar((j % 10) + '0');
 }
 _putchar('\n');
 }

b75cdde5cd884b7b6056716e0e2050a309659ef2
Welde-Ab <35730399+Welde-Ab@users.noreply.github.com>
2022-03-19 02:59:47 +0000

    new

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,34 +1,34 @@
 #include "main.h"
-/**
- * print_number - prints numbers
- * @n: number to be printed
- * Return:void
- */
 
+/**
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
+ */
 void print_number(int n)
 {
-int d = 1, i = 0, ii = 0;
-
+unsigned int m, d, count;
 if (n < 0)
 {
-_putchar('-');
-n = -n;
+_putchar(45);
+m = n * -1;
+}
+else
+{
+m = n;
 }
 
-while (n / d != 0)
-{
-d *= 10;
-i++;
-}
-d = d / 10;
+d = m;
+count = 1;
 
-while (ii < i)
+while (d > 9)
 {
-_putchar('0' + n / d);
-n = n - (n / d) * d;
-d = d / 10;
-ii++;
+d /= 10;
+count *= 10;
 }
-if (i == 0)
-_putchar('0' + n);
+
+for (; count >= 1; count /= 10)
+{
+_putchar(((m / count) % 10) + 48);
+}
 }

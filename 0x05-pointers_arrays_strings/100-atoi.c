c2bd24791ca34f2635fc40edeec4271c4d756226
WeldeAb <35730399+Welde-Ab@users.noreply.github.com>
2022-03-22 20:20:57 -0700

    bonus-1

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,7 +8,7 @@
 
 int _atoi(char *s)
 {
-int sign = 1, resp = 0, firstNum;
+int sign = 1, resp = 0, firstNum, i;
 for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
 {
 if (s[firstNum] == '-')
@@ -16,7 +16,7 @@ if (s[firstNum] == '-')
 sign *= -1;
 }
 }
-for (int i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
+for (i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
 {
 resp *= 10;
 resp += (s[i] - 48);
